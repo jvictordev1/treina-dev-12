7cf38c5dfb3c8760146f476d2bf40fbe3e606de3
jvictordev1 <josevictorcruzrb@gmail.com>
2024-04-02 08:59:08 -0300

    aula 7

--- contact-list/script.js
+++ contact-list/script.js
@@ -1,41 +1,47 @@
-let contactList = [
-  {
-    name: "Andre Schulz",
-    email: "andre@gmail.com",
-    city: "Berlin",
-    country: "Germany",
-    state: "Berlin",
-    photo:
-      "https://cdn.pixabay.com/photo/2024/03/09/02/26/bird-8621836_1280.jpg",
-  },
-  {
-    name: "Fernanda Schulz",
-    email: "fernanda@gmail.com",
-    city: "Dublin",
-    country: "Ireland",
-    state: "Leinster",
-    photo:
-      "https://cdn.pixabay.com/photo/2024/03/09/02/26/bird-8621836_1280.jpg",
-  },
-];
 list = document.getElementById("contact-list");
 loadContacts = document.getElementById("load-contacts");
 
+async function findContacts() {
+  const res = await fetch("https://randomuser.me/api/?results=5");
+  const data = await res.json();
+  console.log(data.results);
+  if (res.status === 200) {
+    let contactList = data.results;
+    contactList.forEach((contact) => {
+      let item = document.createElement("li");
+      item.innerHTML = `
+      <div>
+        <img
+        width="200"
+   			src=${contact.picture.large}
+   			alt="${contact.name.first} ${contact.name.last} photo"
+   			/>
+   			<h2>${contact.name.first} ${contact.name.last}</h2>
+   			<span>${contact.email}</span>
+   			<p>${contact.location.city} | ${contact.location.state} | ${contact.location.country}</p>
+   		</div>
+  	`;
+      list.appendChild(item);
+    });
+  }
+}
+
 loadContacts.addEventListener("click", () => {
-  contactList.forEach((contact) => {
-    let item = document.createElement("li");
-    item.innerHTML = `
-				<div>
-				  <img
-					width="300"
-					src=${contact.photo}
-					alt="${contact.name} photo"
-				  />
-				  <h2>${contact.name}</h2>
-				  <span>${contact.email}</span>
-				  <p>${contact.city} | ${contact.state} | ${contact.country}</p>
-				</div>
-		`;
-    list.appendChild(item);
-  });
+  findContacts();
+  // contactList.forEach((contact) => {
+  //   let item = document.createElement("li");
+  //   item.innerHTML = `
+  // 			<div>
+  // 			  <img
+  // 				width="300"
+  // 				src=${contact.photo}
+  // 				alt="${contact.name} photo"
+  // 			  />
+  // 			  <h2>${contact.name}</h2>
+  // 			  <span>${contact.email}</span>
+  // 			  <p>${contact.city} | ${contact.state} | ${contact.country}</p>
+  // 			</div>
+  // 	`;
+  //   list.appendChild(item);
+  // });
 });
